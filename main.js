69cd63080a83202c29a342c25f7f89afbf2686fc
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-15 21:58:24 -0300

    feat: create space for blank space at vector

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 
+function embaralhaSistema(n, pecas) { 
+    //primeiro aleatoriza a posicao que vai ficar vazia no vetor
+    const indVazio = Math.floor(Math.random() * n);
+    pecas[indVazio] = undefined;
 
-function main(n, ) { 
-
+    //aleatoriza as posicoes das duas cores 
+    for(let i=0; i<n; i++) { 
+        
+    }
 }
+
+//n blocos pretos e n blocos brancos + um espaço vazio
+function main(n) { 
+    const tamSistema = 2*n + 1; 
+    const pecas = [];
+
+    embaralhaSistema(tamSistema, pecas);
+}
+
+main(5);
